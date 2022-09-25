7f69ef10b179044cc554f6aacd1ecec678f3ad1e
Craig <indigomx9@gmail.com>
2022-09-25 14:14:02 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jest and RTL
+
+Initial setup of the project.  Created with TypeScript, Webpack, Testing Library, Jest, and React.
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam asperiores dolorum enim beatae aliquam doloribus dignissimos soluta. Cupiditate corrupti sed et recusandae! Aut molestiae eaque similique voluptatum odit id sed?</p>
+        </React.Fragment>
+    );
+};
+
+
